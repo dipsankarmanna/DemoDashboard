9810c4938c1ffd8ce0adfb4b785883d9eabac5e0
GtechApp <souravsadhukhan.gtech@gmail.com>
2024-02-08 18:26:40 +0530

    dashboard update

--- app/src/main/java/com/example/nidhidashboard/LoginOptionsActivity.java
+++ app/src/main/java/com/example/nidhidashboard/LoginOptionsActivity.java
@@ -1,8 +1,12 @@
 package com.example.nidhidashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.TranslateAnimation;
@@ -21,27 +25,61 @@ public class LoginOptionsActivity extends AppCompatActivity {
 
 
         binding.adminloginbtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
             @Override
             public void onClick(View view) {
                 binding.admincard.setVisibility(View.VISIBLE);
                 binding.empcard.setVisibility(View.GONE);
                 binding.memberCard.setVisibility(View.GONE);
+                binding.loginhead.setText("~Admin Login~");
+
+                binding.adminloginbtn.setTextColor(R.color.btn_back);
+                binding.adminloginbtn.setBackgroundColor(getResources().getColor(R.color.white));
+
+                binding.arrangerloginbtn.setTextColor(R.color.white);
+                binding.arrangerloginbtn.setBackgroundColor(getResources().getColor(R.color.btn_back));
+
+                binding.memberloginbtn.setTextColor(R.color.white);
+                binding.memberloginbtn.setBackgroundColor(getResources().getColor(R.color.btn_back));
             }
         });
         binding.memberloginbtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
             @Override
             public void onClick(View view) {
                 binding.admincard.setVisibility(View.GONE);
                 binding.empcard.setVisibility(View.GONE);
                 binding.memberCard.setVisibility(View.VISIBLE);
+                binding.loginhead.setText("~Member Login~");
+
+                binding.memberloginbtn.setTextColor(R.color.btn_back);
+                binding.memberloginbtn.setBackgroundColor(getResources().getColor(R.color.white));
+
+                binding.adminloginbtn.setTextColor(R.color.white);
+                binding.adminloginbtn.setBackgroundColor(getResources().getColor(R.color.btn_back));
+
+                binding.arrangerloginbtn.setTextColor(R.color.white);
+                binding.arrangerloginbtn.setBackgroundColor(getResources().getColor(R.color.btn_back));
             }
         });
         binding.arrangerloginbtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
             @Override
             public void onClick(View view) {
                 binding.admincard.setVisibility(View.GONE);
                 binding.empcard.setVisibility(View.VISIBLE);
                 binding.memberCard.setVisibility(View.GONE);
+                binding.loginhead.setText("~Executive Login~");
+
+
+                binding.arrangerloginbtn.setTextColor(R.color.btn_back);
+                binding.arrangerloginbtn.setBackgroundColor(getResources().getColor(R.color.white));
+
+                binding.adminloginbtn.setTextColor(R.color.white);
+                binding.adminloginbtn.setBackgroundColor(getResources().getColor(R.color.btn_back));
+
+                binding.memberloginbtn.setTextColor(R.color.white);
+                binding.memberloginbtn.setBackgroundColor(getResources().getColor(R.color.btn_back));
             }
         });
         binding.btnmem.setOnClickListener(new View.OnClickListener() {
